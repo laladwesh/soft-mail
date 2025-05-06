80d39b88d54f74e4c689b38de72b1e50c9f43f2d
Avinash Gupta <guptaavinash302@gmail.com>
2025-05-06 21:26:34 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const cron = require('node-cron');
+const sendReminders = require('./utils/sendReminder.js');
+const dbConnect = require('./lib/mongodb');
+
+dbConnect();
+
+// Run every minute
+cron.schedule('* * * * *', async () => {
+  console.log("Running reminder check...");
+  await sendReminders();
+});
